55df375d88823327e3fb68e49c78e07c63800834
barko <barkhitordtroll@gmail.com>
2023-02-25 00:08:12 +0800

    borrando cosas que no estan en uso

--- app.js
+++ app.js
@@ -2,6 +2,31 @@
 let account = document.querySelector('.init')
 let mp = document.querySelector('.mp')
 
+let win= undefined;
+window.addEventListener('resize',()=>{
+	win = window.innerWidth;
+	if (win < 420) {
+	account.innerHTML = ''
+	account.classList.replace('nav_texto','account')
+
+}else if (win > 420) {
+	account.innerHTML = 'Iniciar Sesion'
+	account.classList.replace('account','nav_texto')
+}
+})
+
+window.addEventListener('resize',()=>{
+	win = window.innerWidth;
+	if (win < 420) {
+	mp.innerHTML = ''
+	mp.classList.replace('nav_texto','mainpage')
+
+}else if (win > 420) {
+	mp.innerHTML = 'Iniciar Sesion'
+	mp.classList.replace('mainpage','nav_texto')
+}
+})
+
 
 let buttonr = document.querySelector('.buttonr')
 let buttonl = document.querySelector('.buttonl')
@@ -193,37 +218,6 @@ if (activeregister) {
 
 }
 })
-
-
-
-
-
-
-let win= undefined;
-window.addEventListener('resize',()=>{
-	win = window.innerWidth;
-	if (win < 420) {
-	account.innerHTML = ''
-	account.classList.replace('nav_texto','account')
-
-}else if (win > 420) {
-	account.innerHTML = 'Iniciar Sesion'
-	account.classList.replace('account','nav_texto')
-}
-})
-
-window.addEventListener('resize',()=>{
-	win = window.innerWidth;
-	if (win < 420) {
-	mp.innerHTML = ''
-	mp.classList.replace('nav_texto','mainpage')
-
-}else if (win > 420) {
-	mp.innerHTML = 'Iniciar Sesion'
-	mp.classList.replace('mainpage','nav_texto')
-}
-})
-
 
 // las condicionales son funciones(las funciones son cosas que hacen cosas)
 // son funciones que ejecutan su codigo interior SOLO SI
